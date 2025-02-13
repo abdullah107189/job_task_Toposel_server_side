13d54935d8473e682e8bb0cdbb5712f52595073a
abdullah107189 <abdullah107189@gmail.com>
2025-02-14 01:01:09 +0600

    create post api for add user info on the mongodb

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 4545;
+const port = process.env.PORT || 4545;
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:<db_password>@cluster0.fx40ttv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fx40ttv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,11 +26,24 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 
+    const database = client.db("job_task_Toposel");
+    const userInfoCollection = database.collection("user_info");
+
     app.get("/", (req, res) => {
       res.send("Hello World!");
     });
 
+    // user info add in the db
+    app.post("/add-user", async (req, res) => {
+      const userInfo = req.body;
+      const result = await userInfoCollection.insertOne(userInfo);
+      res.send(result);
+    });
 
+    app.get('/login-user', async(req, res) =>{
+        const email = req.body;
+        console.log(email);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +51,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
